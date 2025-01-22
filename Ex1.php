cde8a237cefa3031e9bb4e08aad70c23f8ff094d
Garfield-36 <garfieldzaa555@gmail.com>
2025-01-22 11:30:37 +0700

    week4

---
+++ Ex1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Anuphan:wght@100..700&family=Charm:wght@400;700&family=Pridi:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <br>การเขียนโปรเเกรมด้วย PHP <br>
+   <br>664485003 กิตดนัย นราแก้ว<br>
+    หมู่เรียน 66/96 <br>
+</body>
+</html>
